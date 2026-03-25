11de90fdee40e9f7dcf63e72ecbec8db7fb7a89b
Malte Neemann <malteneemann129@gmail.com>
2026-03-25 11:11:31 +0100

    Added errorhandling for reading and saving the file. Removed redundant code and created helper functions

--- app.py
+++ app.py
@@ -3,18 +3,49 @@ import json
 
 app = Flask(__name__)
 
+def get_posts():
+    """Helper function: Reads the Data and returns it"""
+
+    try:
+        with open("data.json", "r") as file:
+            blog_posts = json.load(file)
+
+        return blog_posts
+
+    except (FileNotFoundError, json.JSONDecodeError):
+        return None
+
+
+def save_posts(blog_posts):
+    """Helpfer function: Saves the new Posting-Data in our json file"""
+
+    try:
+        with open("data.json", "w") as file:
+            json.dump(blog_posts, file, indent=4)
+
+    except OSError:
+        return None
+
 @app.route('/')
 def index():
-    with open("data.json", "r") as file:
-        blog_posts = json.load(file)
+    """Shows the user the 'landing-page'. Where he can see all the posts and options"""
+
+    #Get all the blog_posts
+    blog_posts = get_posts()
+    if blog_posts is None:
+        return "We had an Error handling this file", 404
+
     return render_template('index.html', posts=blog_posts)
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
+    """Adds a new blog-post to our blog"""
+
     if request.method == 'POST':
         #Read the file
-        with open("data.json", "r") as file:
-            blog_posts = json.load(file)
+        blog_posts = get_posts()
+        if blog_posts is None:
+            return "We had an Error handling this file", 404
 
         #Get the information from the Form
         author = request.form.get('author')
@@ -33,8 +64,7 @@ def add():
         blog_posts.append(new_dict)
 
         #Write new file with updated info
-        with open("data.json", "w") as file:
-            json.dump(blog_posts, file, indent=4)
+        save_posts(blog_posts)
 
         return redirect(url_for('index'))
 
@@ -42,19 +72,26 @@ def add():
 
 @app.route("/delete/<int:post_id>")
 def delete(post_id):
-    with open("data.json", "r") as file:
-        blog_posts = json.load(file)
+    """Deletes a post from our blog"""
+
+    #Get the data
+    blog_posts = get_posts()
+    if blog_posts is None:
+        return "We had an Error handling this file", 404
 
     blog_posts = [post for post in blog_posts if post['id'] != post_id]
 
-    with open("data.json", "w") as file:
-        json.dump(blog_posts, file, indent=4)
+    save_posts(blog_posts)
 
     return redirect(url_for('index'))
 
 def fetch_post_by_id(post_id):
-    with open("data.json", "r") as file:
-        blog_posts = json.load(file)
+    """Helper function to get the right post by id"""
+
+    #Get the data
+    blog_posts = get_posts()
+    if blog_posts is None:
+        return None
 
     for post in blog_posts:
         if post["id"] == post_id:
@@ -64,10 +101,16 @@ def fetch_post_by_id(post_id):
 
 @app.route('/update/<int:post_id>', methods=['GET', 'POST'])
 def update(post_id):
+    """Updates a post with new data, everything which is not updated
+    will remain the same"""
+
+    #Get the searched post
     post = fetch_post_by_id(post_id)
 
-    with open("data.json", "r") as file:
-        blog_posts = json.load(file)
+    #Get the data
+    blog_posts = get_posts()
+    if blog_posts is None:
+        return "We had an Error handling this file", 404
 
     if post is None:
         return "Post not found", 404
@@ -83,8 +126,7 @@ def update(post_id):
                 blog_post["title"] = title or blog_post["title"]
                 blog_post["content"] = content or blog_post["content"]
 
-        with open("data.json", "w") as file:
-            json.dump(blog_posts, file, indent=4)
+        save_posts(blog_posts)
 
         return redirect(url_for('index'))
 
@@ -92,3 +134,8 @@ def update(post_id):
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
+
+
+
+
+
